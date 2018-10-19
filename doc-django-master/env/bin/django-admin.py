af22f9652534cec4ee870d504fb16bcbeb4628d3
zyy123jy <yz2377@cornell.edu>
2018-10-19 18:13:01 -0400

    First commit

---
+++ doc-django-master/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/zyy/Documents/kdd-master/myweb/doc-django-master/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
